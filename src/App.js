b26c6103ddc01ab8ab3b251fcc81735074167ac6
“shreyaansjain06” <shreyaansjain60@gmail.com>
2022-05-08 11:21:48 +0530

    Add components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Sidebar from './Components/Sidebar';
-import Navbar from './Components/Navbar'; 
+import Navbar from './Components/Navbar';
+import Chartdata from './Components/Chartdata';
 import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
       <Sidebar/>
     <Navbar/>
+    <Chartdata/>
     </div>
   );
 }
